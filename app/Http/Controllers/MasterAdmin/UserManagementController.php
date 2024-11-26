2ef2f4781cbbe04bf62ec9095b784a3aab3048f9
Muhammad Awais <muhammadawaisjkl@gmail.com>
2024-11-27 03:32:50 +0500

    update

---
+++ app/Http/Controllers/MasterAdmin/UserManagementController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\MasterAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserManagementController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/UserAdmin/AdController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\UserAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdController extends Controller
+{
+    //
+}
