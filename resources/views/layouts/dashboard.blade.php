df71dc9901c023312b37810da3877f530c70b5fb
Muhammad Awais <muhammadawaisjkl@gmail.com>
2024-12-16 02:44:42 +0500

    test

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,10 +6,10 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-overrides.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap-overrides.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/dashboard.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/plus-jakarta-display.min.css">
-    <link rel="stylesheet" href="{{ asset('assets/css/color-scheme.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/color-scheme.css') }}">
 </head>
 
 @include('components.dashboard-theme-script')
